ba559e3ab38a34c1965b66852899679fe2cff31b
ghaidafasya24 <ghaidafasya5@gmail.com>
2022-12-29 16:05:08 +0700

    Praktikum 9

---
+++ proses_loggin.php
@@ -0,0 +1,13 @@
+<?php 
+session_start();
+if(isset($HTTP_SESSION_VARS["session_status"]))
+{
+    echo("<pre>");
+    echo("Username : <b>".$HTTP_SESSION_VARS["session_user"]."</b><br>");
+    echo("Password : <b>".$HTTP_SESSION_VARS["session_password"]."</b><br>");
+    echo("Click <a href=\"loggout.php\">Loggout</a> untuk keluar !");
+    echo("</pre>");
+}else
+    header("Location: loggin.php");
+    exit
+?>
